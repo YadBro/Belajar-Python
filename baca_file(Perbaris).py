2e3cec0b0cc7fb2d849b220f425191f82f1dedd8
YadBro <83240362+YadBro@users.noreply.github.com>
2021-10-15 22:17:11 +0700

    Delete

--- baca_file(Perbaris).py
+++
@@ -1,5 +0,0 @@
-file_puisi = open("Read File In Python/puisi.txt", "r")
-puisi = file_puisi.readlines()  # MEMBACA PERBARIS (outputnya jadi tuple string)
-print(puisi[0])  # MEMANGGIL BARIS 1
-print(puisi[1])  # MEMANGGIL BARIS 2
-file_puisi.close()
